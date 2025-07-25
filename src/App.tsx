be27c58c53f3ac5169dfb06c8b1efca98e0f7bf9
Leo Nickl <leo@leonickl.de>
2025-07-25 23:04:46 +0200

    sync on app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { ToastContainer } from "react-toastify";
 import MapView from "./components/MapView";
+import { useEffect } from "react";
+import { updateList, workQueue } from "./utils/storage";
 
 export default function App() {
+    useEffect(() => {
+        updateList();
+        workQueue();
+    }, []);
+
     return (
         <main style={{ height: "100vh", width: "100vw" }}>
             <MapView />
