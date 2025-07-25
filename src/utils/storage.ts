6f2414f96b7ee77d4456e205d0b8265bbb5e968e
Leo Nickl <leo@leonickl.de>
2025-07-25 17:40:31 +0200

    push and pull

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,6 +3,8 @@ export type MarkerData = {
     name: string,
     lat: number,
     lng: number,
+    category?: string;
+    comment?: string;
 }
 
 export function getItems() : MarkerData[]{
@@ -12,10 +14,46 @@ export function getItems() : MarkerData[]{
 }
 
 export function addItem(newItem: MarkerData) {
-    return localStorage.setItem("markers", JSON.stringify([...getItems(), newItem]))
+    localStorage.setItem("markers", JSON.stringify([...getItems(), newItem]))
+
+    pushItem(newItem).then()
 }
 
+const BASE_URL = `${import.meta.env.VITE_NC_URL}/index.php/apps/maps/api/1.0/favorites`;
+const AUTH_HEADER = 'Basic ' + btoa(`${import.meta.env.VITE_NC_USER}:${import.meta.env.VITE_NC_PASS}`);
 
-export function pullItems() {}
+export async function pullItems(): Promise<MarkerData[]> {
+    const res = await fetch(BASE_URL, {
+        method: 'GET',
+        headers: {
+            Authorization: AUTH_HEADER,
+        },
+    });
 
-export function pushItems() {}
+    if (!res.ok) {
+        throw new Error(`Failed to pull markers: ${res.status}`);
+    }
+
+    const data = await res.json();
+
+    return data as MarkerData[];
+}
+
+export async function pushItem(marker: MarkerData): Promise<MarkerData> {
+    const url = `${BASE_URL}`;
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: AUTH_HEADER,
+        },
+        body: JSON.stringify(marker),
+    });
+
+    if (!res.ok) {
+        throw new Error(`Failed to push marker: ${res.status}`);
+    }
+
+    return res.json() as Promise<MarkerData>;
+}
